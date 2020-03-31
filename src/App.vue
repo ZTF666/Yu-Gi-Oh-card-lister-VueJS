dca5dca23d1453883e2daab3da7372afe6d895d7
ZTF <ZTF666@protonmail.ch>
2020-03-31 11:44:29 +0100

    colors mate colors

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
-    <div class="grid-container">
+    <div class="search">
       <form @submit.prevent="callApi">
         <input type="text" v-model="query" />
         <button>Go</button>
       </form>
     </div>
+
     <div class="grid-container">
       <div v-for="card in cards" :key="card.id" class="grid-item">
         <img :src="card.card_images[0].image_url" width="150" height="265" />
@@ -17,6 +18,7 @@
 
 <script>
 export default {
+  name: "app",
   data: () => {
     return {
       api_endPoint: "https://db.ygoprodeck.com/api/v5/cardinfo.php?fname=",
@@ -38,6 +40,7 @@ export default {
               this.cards.push(card);
             });
             console.log(data);
+            this.query = "";
           });
       }
     }
@@ -46,23 +49,35 @@ export default {
 </script>
 
 <style>
+body {
+  background: linear-gradient(
+    54deg,
+    rgba(2, 0, 36, 1) 0%,
+    #c31432 33%,
+    #240b36 100%
+  );
+}
 .grid-container {
   grid-template-columns: repeat(auto-fit, minmax(414px, 1fr));
   display: grid;
-  background-color: #2196f3;
+  background-color: transparent;
   padding: 5px;
   grid-gap: 5px;
   width: 100%;
 }
 .grid-item {
-  background-color: rgba(255, 255, 255, 0.8);
+  background-color: transparent;
   border: 1px solid rgba(0, 0, 0, 0.8);
   padding: 20px;
-  font-size: 10px;
+  font-size: 15px;
   text-align: center;
   border-radius: 5px;
   cursor: pointer;
-
+  color: aliceblue;
   box-shadow: 0 15px 30px rgba(0, 0, 0, 0.2), 0 10px 10px rgba(0, 0, 0, 0.2);
+}
+.search {
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
